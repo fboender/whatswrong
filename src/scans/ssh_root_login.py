632541b5b1e168a54f9de6a1ae2d2b2edf6d2072
Ferry Boender <ferry.boender@gmail.com>
2014-10-23 07:47:18 +0200

    ssh::root_login: Improved test

--- src/scans/ssh_root_login.py
+++ src/scans/ssh_root_login.py
@@ -10,7 +10,7 @@ __explanation__ = ''
 def scan():
     fc = file('/etc/ssh/sshd_config', 'r').read().lower()
     matches = re.findall('.*permitrootlogin.*', fc)
-    if matches and matches[0].endswith('yes'):
+    if matches and matches[0].strip().endswith('yes'):
         return (scanner.FAIL, 'SSH allows remote root logins')
     else:
         return (scanner.PASS, 'SSH does not allow remote root logins')
