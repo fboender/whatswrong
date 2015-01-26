c54a9d3c59e0b2c41165f43095078b1a2142f2b3
Ferry Boender <ferry.boender@gmail.com>
2015-01-26 22:03:26 +0100

    mysql_no_root_pw scan: exit mysql shell in case there is no password set.

--- src/scans/mysql_no_root_pw.py
+++ src/scans/mysql_no_root_pw.py
@@ -9,7 +9,7 @@ __explanation__ = '''The MySQL server's root account does not have a password
 set. This is commonly a initial setup mistake.'''
 
 def scan():
-    res = tools.cmd('mysql -u root -h 127.0.0.1')
+    res = tools.cmd('mysql -u root -h 127.0.0.1 -e "exit" ')
     if 'access denied' in res['stderr'].lower():
         return scanner.Result(scanner.PASS, 'The MySQL root account has a password')
     return scanner.Result(scanner.FAIL, 'The MySQL root account has no password')
